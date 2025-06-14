7702d644b2425bd9933258e2a69a64411c77df14
Peaches_MLG <PeachesMLG100@gmail.com>
2025-06-14 18:05:38 +0100

    Initial Commit

---
+++ src/index.ts
@@ -0,0 +1,43 @@
+import {
+  ActionRowBuilder,
+  ChatInputCommandInteraction,
+  Client,
+  GatewayIntentBits,
+  Interaction,
+  ModalBuilder,
+  TextInputBuilder,
+  TextInputStyle,
+} from 'discord.js';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessages,
+  ],
+});
+
+const bannedWords = ["frfr", "fr fr"];
+
+client.once('ready', async () => {
+  console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if(message.author.id === '706821340073689179'){
+    for (const bannedWord of bannedWords) {
+      if(message.content.toLowerCase().includes(bannedWord.toLowerCase())){
+        await message.delete();
+        await message.channel.send("<@706821340073689179> nu uh")
+        return;
+      }
+    }
+  }
+});
+
+client.login(process.env.DISCORD_TOKEN).catch((error) => {
+  console.error('Error logging in:', error);
+});
