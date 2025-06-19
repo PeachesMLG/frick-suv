393de1bdf4ab949a0b301bda54925dfc5b76f572
Peaches_MLG <PeachesMLG100@gmail.com>
2025-06-19 17:42:19 +0100

    Removed Uncode shit

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,43 @@
-import {
-  ActionRowBuilder,
-  ChatInputCommandInteraction,
-  Client,
-  GatewayIntentBits,
-  Interaction,
-  ModalBuilder,
-  TextInputBuilder,
-  TextInputStyle,
-} from 'discord.js';
+import {Client, GatewayIntentBits, TextInputStyle,} from 'discord.js';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
 
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMessages,
-  ],
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildMessages,
+    ],
 });
 
 const bannedWords = ["frfr"];
 const bannedUsers = ['706821340073689179'];
 
 client.once('ready', async () => {
-  console.log(`Logged in as ${client.user?.tag}!`);
+    console.log(`Logged in as ${client.user?.tag}!`);
 });
 
 client.on('messageCreate', async (message) => {
-  for (const user of bannedUsers){
-    if(message.author.id === user){
-      for (const bannedWord of bannedWords) {
-        if(message.content.toLowerCase().replace(/[\s.,\-;]+/g, "").includes(bannedWord.toLowerCase())) {
-          await message.delete();
-          await message.channel.send(`<@${user}> nu uh`)
-          return;
+    for (const user of bannedUsers) {
+        if (message.author.id === user) {
+            for (const bannedWord of bannedWords) {
+                if (
+                    message.content
+                        .toLowerCase()
+                        .replace(/[\s.,\-;]+/g, "")
+                        .replace(/[^\x00-\x7F]/g, "")
+                        .includes(bannedWord.toLowerCase())
+                ) {
+                    await message.delete();
+                    await message.channel.send(`<@${user}> nu uh`)
+                    return;
+                }
+            }
         }
-      }
     }
-  }
 });
 
 client.login(process.env.DISCORD_TOKEN).catch((error) => {
-  console.error('Error logging in:', error);
+    console.error('Error logging in:', error);
 });
